778e60f0efef8b7a806f5111c6ced7fdef2fdc2e
Christopher Serr <christopher.serr@gmail.com>
2023-10-24 23:05:24 +0200

    Remove the OS limit for `rustls` targets (#43)
    It doesn't make sense to limit the OS to Linux, macOS, and Windows
    because `rustls` supports more than that.

--- src/platform/native.rs
+++ src/platform/native.rs
@@ -1,38 +1,32 @@
 use http::{Request, Response};
 use hyper::body::Buf;
 
-#[cfg(all(
-    any(target_os = "linux", target_family = "windows", target_os = "macos"),
-    any(
-        target_arch = "arm",
-        target_arch = "aarch64",
-        target_arch = "x86",
-        target_arch = "x86_64",
-        target_arch = "loongarch64",
-        all(target_arch = "mips", target_endian = "little"),
-        all(target_arch = "mips64", target_endian = "little"),
-        all(target_arch = "powerpc", target_endian = "big"),
-        target_arch = "powerpc64",
-        target_arch = "riscv64",
-        target_arch = "s390x",
-    ),
+#[cfg(any(
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "x86",
+    target_arch = "x86_64",
+    target_arch = "loongarch64",
+    all(target_arch = "mips", target_endian = "little"),
+    all(target_arch = "mips64", target_endian = "little"),
+    all(target_arch = "powerpc", target_endian = "big"),
+    target_arch = "powerpc64",
+    target_arch = "riscv64",
+    target_arch = "s390x",
 ))]
 use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
-#[cfg(not(all(
-    any(target_os = "linux", target_family = "windows", target_os = "macos"),
-    any(
-        target_arch = "arm",
-        target_arch = "aarch64",
-        target_arch = "x86",
-        target_arch = "x86_64",
-        target_arch = "loongarch64",
-        all(target_arch = "mips", target_endian = "little"),
-        all(target_arch = "mips64", target_endian = "little"),
-        all(target_arch = "powerpc", target_endian = "big"),
-        target_arch = "powerpc64",
-        target_arch = "riscv64",
-        target_arch = "s390x",
-    ),
+#[cfg(not(any(
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "x86",
+    target_arch = "x86_64",
+    target_arch = "loongarch64",
+    all(target_arch = "mips", target_endian = "little"),
+    all(target_arch = "mips64", target_endian = "little"),
+    all(target_arch = "powerpc", target_endian = "big"),
+    target_arch = "powerpc64",
+    target_arch = "riscv64",
+    target_arch = "s390x",
 )))]
 use hyper_tls::HttpsConnector;
 use std::{io::Read, ops::Deref};
@@ -53,42 +47,36 @@ pub struct Client {
 
 impl Client {
     pub fn new() -> Self {
-        #[cfg(all(
-            any(target_os = "linux", target_family = "windows", target_os = "macos"),
-            any(
-                target_arch = "arm",
-                target_arch = "aarch64",
-                target_arch = "x86",
-                target_arch = "x86_64",
-                target_arch = "loongarch64",
-                all(target_arch = "mips", target_endian = "little"),
-                all(target_arch = "mips64", target_endian = "little"),
-                all(target_arch = "powerpc", target_endian = "big"),
-                target_arch = "powerpc64",
-                target_arch = "riscv64",
-                target_arch = "s390x",
-            ),
+        #[cfg(any(
+            target_arch = "arm",
+            target_arch = "aarch64",
+            target_arch = "x86",
+            target_arch = "x86_64",
+            target_arch = "loongarch64",
+            all(target_arch = "mips", target_endian = "little"),
+            all(target_arch = "mips64", target_endian = "little"),
+            all(target_arch = "powerpc", target_endian = "big"),
+            target_arch = "powerpc64",
+            target_arch = "riscv64",
+            target_arch = "s390x",
         ))]
         let https = HttpsConnectorBuilder::new()
             .with_native_roots()
             .https_only()
             .enable_http2()
             .build();
-        #[cfg(not(all(
-            any(target_os = "linux", target_family = "windows", target_os = "macos"),
-            any(
-                target_arch = "arm",
-                target_arch = "aarch64",
-                target_arch = "x86",
-                target_arch = "x86_64",
-                target_arch = "loongarch64",
-                all(target_arch = "mips", target_endian = "little"),
-                all(target_arch = "mips64", target_endian = "little"),
-                all(target_arch = "powerpc", target_endian = "big"),
-                target_arch = "powerpc64",
-                target_arch = "riscv64",
-                target_arch = "s390x",
-            ),
+        #[cfg(not(any(
+            target_arch = "arm",
+            target_arch = "aarch64",
+            target_arch = "x86",
+            target_arch = "x86_64",
+            target_arch = "loongarch64",
+            all(target_arch = "mips", target_endian = "little"),
+            all(target_arch = "mips64", target_endian = "little"),
+            all(target_arch = "powerpc", target_endian = "big"),
+            target_arch = "powerpc64",
+            target_arch = "riscv64",
+            target_arch = "s390x",
         )))]
         let https = HttpsConnector::new();
         let client = hyper::Client::builder().build::<_, hyper::Body>(https);
